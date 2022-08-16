2fa6c915d9fbf8d8e91070439d4346a855ee13c2
richardly9319 <39727690+richardly9319@users.noreply.github.com>
2022-08-15 23:48:31 -0700

    13th commit

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AiFillEye, AiFillGithub } from 'react-icons/ai';
+import { AiFillEye, AiFillGithub, AiOutlineGithub } from 'react-icons/ai';
 import { motion } from 'framer-motion';
 
 import { AppWrap, MotionWrap } from '../../wrapper';
@@ -21,10 +21,19 @@ const Work = () => {
       <h2 className="head-text">Project <span>Portfolio</span></h2>
       <br></br>
       <br></br>
-      <div>
+      <div className="projects">
+
+      <a href="https://github.com/richardly9319/tasktracker" target='_blank' rel="noreferrer">
+      <div className="github">
+      <h3>Task Tracker App</h3>
+      <AiOutlineGithub />
+      </div>
+      </a>
+      
       
       <motion.div whileHover={{ scale: 1.1 }} transition={{ duration: 0.5, type: 'tween' }}>
-      <h3 align="center">Task Tracker App</h3>
+      
+      
       <a href="https://tasktracker-delta.vercel.app/" target='_blank' rel="noreferrer">
       <img src={images.tasktracker} alt='Task Tracker' height={180} width={320} className="portfolio_images">
       </img>
@@ -32,8 +41,14 @@ const Work = () => {
       </motion.div>
       <br></br>
 
+      <a href="https://github.com/richardly9319/ecommerce" target='_blank' rel="noreferrer">
+      <div className="github">
+      <h3>E-Commerce Store</h3>
+      <AiOutlineGithub />
+      </div>
+      </a>
+
       <motion.div whileHover={{ scale: 1.1 }} transition={{ duration: 0.5, type: 'tween' }}>
-      <h3 align="center">E-Commerce Store</h3>
       <a href="https://ecommerce-kfp1f1px0-richardly9319.vercel.app/" target='_blank' rel="noreferrer">
       <img src={images.ecommercesite} alt='E-Commerce' height={180} width={320} className="portfolio_images">
       </img>
@@ -41,8 +56,14 @@ const Work = () => {
       </motion.div>
       <br></br>
 
+      <a href="https://github.com/richardly9319/rly_realestate" target='_blank' rel="noreferrer">
+      <div className="github">
+      <h3>Real Estate Listings from API</h3>
+      <AiOutlineGithub />
+      </div>
+      </a>
+
       <motion.div whileHover={{ scale: 1.1 }} transition={{ duration: 0.5, type: 'tween' }}>
-      <h3 align="center">Real Estate Listings from API</h3>
       <a href="https://rly-realestate-hhct9hgqh-richardly9319.vercel.app/" target='_blank' rel="noreferrer">
       <img src={images.realestatesite} alt='Real estate site' height={180} width={320} className="portfolio_images">
       </img>
@@ -50,13 +71,20 @@ const Work = () => {
       </motion.div>
       <br></br>
 
+      <a href="https://github.com/richardly9319/rly_portfolio" target='_blank' rel="noreferrer">
+      <div className="github">
+      <h3>Portfolio Website</h3>
+      <AiOutlineGithub />
+      </div>
+      </a>
+
       <motion.div whileHover={{ scale: 1.1 }} transition={{ duration: 0.5, type: 'tween' }}>
-      <h3 align="center">Portfolio Website</h3>
-      <a href="http://localhost:3000/" target='_blank' rel="noreferrer">
+      <a href="https://rly-portfolio-48dlh0w9e-richardly9319.vercel.app/" target='_blank' rel="noreferrer">
       <img src={images.portfoliosite} alt='Portfolio' height={180} width={320} className="portfolio_images">
       </img>
       </a>
       </motion.div>
+
       </div>
 
       
