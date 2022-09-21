13eee025da412768b073beeb91f8ae9bfca34d22
richardly9319 <39727690+richardly9319@users.noreply.github.com>
2022-09-21 16:26:56 -0700

    18.0

--- src/container/Education/Education.jsx
+++ src/container/Education/Education.jsx
@@ -15,25 +15,12 @@ const Education = () => {
       <h3 className="head-text">Education & Certifications</h3>
 
       <div className="app__profiles">
-        
-          <motion.div whileHover={{ scale: 1.1 }} transition={{ duration: 0.5, type: 'tween' }}
-            className="app__profile-item"
-          >
-            <a href="https://www.coursera.org/professional-certificates/tensorflow-in-practice" target='_blank' rel="noreferrer" className="education_links">
-            <img src={images.coursera} alt='coursera' />
-            <h2 className="bold-text" style={{ marginTop: 20 }}>
-              
-                DeepLearning.AI TensorFlow Developer Specialization
-              
-              </h2>
-              </a>
-            <p className="education_desc" style={{ marginTop: 10 }}>Issued - August 2020</p>
-          </motion.div>
 
-          <motion.div whileHover={{ scale: 1.1 }} transition={{ duration: 0.5, type: 'tween' }}
+
+           <motion.div whileHover={{ scale: 1.1 }} transition={{ duration: 0.5, type: 'tween' }}
             className="app__profile-item"
             
-          >
+            >
             
             <a href="https://www.cs.ubc.ca/" target='_blank' rel="noreferrer" className="education_links">
             <img src={images.ubc} alt='ubc' />
@@ -42,6 +29,37 @@ const Education = () => {
             <br></br>
             <p className="education_desc" style={{ marginTop: 10 }}>(Incomplete) 2011-2014</p>
           </motion.div>
+        
+          <motion.div whileHover={{ scale: 1.1 }} transition={{ duration: 0.5, type: 'tween' }}
+            className="app__profile-item"
+          >
+            <a href="https://www.coursera.org/account/accomplishments/professional-cert/ECEPS2J9F7TE" target='_blank' rel="noreferrer" className="education_links">
+            <img src={images.deeplearning} alt='deeplearning' />
+            <h2 className="bold-text" style={{ marginTop: 20 }}>
+              
+                DeepLearning.AI: TensorFlow Developer Specialization
+              
+              </h2>
+              </a>
+            <p className="education_desc" style={{ marginTop: 10 }}>Issued - August 2020</p>
+          </motion.div>
+
+      
+
+          <motion.div whileHover={{ scale: 1.1 }} transition={{ duration: 0.5, type: 'tween' }}
+            className="app__profile-item"
+            
+          >
+            
+            <a href="https://www.coursera.org/account/accomplishments/verify/B5JKUV9FDA45" target='_blank' rel="noreferrer" className="education_links">
+            <img src={images.meta} alt='ubc' />
+            <h2 className="bold-text" style={{ marginTop: 20 }}>Introduction to <br/>Front-End Development</h2>
+            </a>
+            <br></br>
+            <p className="education_desc" style={{ marginTop: 10 }}>Issued - August 2022</p>
+          </motion.div>
+
+
         
       </div>
     </>
